92c9bb9443c47ba35fb07cf0a6afa387d0297c7b
junghyunsoo24 <sunni303356@naver.com>
2023-05-30 00:15:38 +0900

    swing을 자바fx로구현및 리스트확인

--- src/main/java/Entity/DongDataAnalysis.java
+++ src/main/java/Entity/DongDataAnalysis.java
@@ -1,43 +1,44 @@
 package Entity;
 
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.StandardChartTheme;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.data.category.DefaultCategoryDataset;
-
-import javax.swing.JFrame;
-import java.awt.Font;
 import java.text.DecimalFormat;
 import java.util.*;
 
-public class DongDataAnalysis extends JFrame {
+public class DongDataAnalysis extends Application {
     //법정동과 이용금액을 저장
     TreeMap<String, Double> dongAmountMap = new TreeMap<>();
 
     //이용금액 출력 포맷
     DecimalFormat decimalFormat = new DecimalFormat("#,##0");
 
-    // 그래프 그리기 위한 데이터셋 생성
-    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
     private String dongName = ""; // 법정동명
     private double amount = 0.0; // 이용금액
 
-    public DongDataAnalysis() {
-        // JFrame에 ChartPanel 추가
-        Font font = new Font("맑은 고딕", Font.BOLD, 5);
-        StandardChartTheme theme = new StandardChartTheme("Korean");
-        theme.setExtraLargeFont(font);
-        theme.setLargeFont(font);
-        theme.setRegularFont(font);
-        theme.setSmallFont(font);
-        ChartFactory.setChartTheme(theme);
+    private XYChart.Series<String, Number> series = new XYChart.Series<>();
+    private ObservableList<XYChart.Series<String, Number>> chartData;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox root = new VBox();
+
+        // 데이터셋 생성
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+        chartData = barChart.getData();
 
         // CSV 파일 로드
         try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\sunni\\OneDrive\\바탕 화면\\창의프로젝트\\유성구_법정동별 소비금액 데이터_2020.csv"))) {
@@ -58,13 +59,11 @@ public class DongDataAnalysis extends JFrame {
                 String check = values[4];
                 dongName = values[3]; // 법정동명
 
-                if(check.equals("A")||check.equals("D")||check.equals("M")||check.equals("N")||check.equals("O")||check.equals("R")||check.equals("S")){
+                if (check.equals("A") || check.equals("D") || check.equals("M") || check.equals("N") || check.equals("O") || check.equals("R") || check.equals("S")) {
                     amount = Double.parseDouble(values[7]);
-                }
-                else if(check.equals("E")) {
+                } else if (check.equals("E")) {
                     amount = Double.parseDouble(values[8]);
-                }
-                else {
+                } else {
                     amount = Double.parseDouble(values[6]);
                 }
 
@@ -76,7 +75,6 @@ public class DongDataAnalysis extends JFrame {
                     // 법정동명이 처음 나온 경우, 새로운 항목으로 추가
                     dongAmountMap.put(dongName, amount);
                 }
-
             }
 
             // TreeMap의 엔트리를 순회하며 데이터셋에 값을 추가
@@ -85,62 +83,56 @@ public class DongDataAnalysis extends JFrame {
                 double amount = entry.getValue();
 
                 // 데이터셋에 값 추가
-                dataset.setValue(amount, "2020년 법정동별 이용 금액", dongName);
+                series.getData().add(new XYChart.Data<>(dongName, amount));
             }
+
+            // 데이터셋을 차트에 추가
+            chartData.add(series);
 
             // 엔트리를 List에 저장
             List<Map.Entry<String, Double>> entryList = new ArrayList<>(dongAmountMap.entrySet());
 
             // 엔트리를 금액을 기준으로 내림차순 정렬
-            Collections.sort(entryList, (entry1, entry2) -> Double.compare(entry2.getValue(), entry1.getValue()));
+            entryList.sort((entry1, entry2) -> Double.compare(entry2.getValue(), entry1.getValue()));
 
+            // 엔트리를 출력
+            for (Map.Entry<String, Double> entry : entryList) {
+                String dongName = entry.getKey();
+                double amount = entry.getValue();
+                String formattedAmount = decimalFormat.format(amount);
+                System.out.println(dongName + ": " + formattedAmount + "원");
+            }
 
             // 최댓값 출력
             Map.Entry<String, Double> maxEntry = entryList.get(0);
             String maxDongName = maxEntry.getKey();
             double maxAmount = maxEntry.getValue();
             String formattedMaxAmount = decimalFormat.format(maxAmount);
-            System.out.println("가장 많이 소비한 동은 " + maxDongName + "에 " + formattedMaxAmount + "원 입니다.");
+            Label maxLabel = new Label("가장 많이 소비한 동은 " + maxDongName + "에 " + formattedMaxAmount + "원 입니다.");
 
             // 최솟값 출력
             Map.Entry<String, Double> minEntry = entryList.get(entryList.size() - 1);
             String minDongName = minEntry.getKey();
             double minAmount = minEntry.getValue();
             String formattedMinAmount = decimalFormat.format(minAmount);
-            System.out.println("가장 적게 소비한 동은 " + minDongName + "에 " + formattedMinAmount + "원 입니다.");
+            Label minLabel = new Label("가장 적게 소비한 동은 " + minDongName + "에 " + formattedMinAmount + "원 입니다.");
 
-            // 차트 생성
-            JFreeChart chart = ChartFactory.createBarChart(
-                    "2020년 법정동별 이용 금액", // 차트 제목
-                    "법정동명", // X축 레이블
-                    "이용 금액", // Y축 레이블
-                    dataset // 데이터셋
-            );
+            // root에 컴포넌트 추가
+            root.getChildren().addAll(barChart, maxLabel, minLabel);
 
-            // X축 데이터의 라벨을 법정동명으로 설정
-            chart.getCategoryPlot().getDomainAxis().setLabelFont(new Font("맑은 고딕", Font.BOLD, 12));
+            // Scene 생성
+            Scene scene = new Scene(root, 800, 600);
 
-            // Y축 데이터의 라벨을 이용금액으로 설정
-            chart.getCategoryPlot().getRangeAxis().setLabelFont(new Font("맑은 고딕", Font.BOLD, 12));
-
-            // Y축 데이터의 레이블 포맷 설정
-            NumberAxis rangeAxis = (NumberAxis) chart.getCategoryPlot().getRangeAxis();
-            rangeAxis.setNumberFormatOverride(decimalFormat);
-
-            // ChartPanel 객체 생성 및 지정
-            ChartPanel chartPanel = new ChartPanel(chart);
-            add(chartPanel);
-            setTitle("2020년 법정동별 이용 금액");
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-            setSize(500, 480);
-            setLocationRelativeTo(null);
-            setVisible(true);
+            // Stage 설정
+            primaryStage.setTitle("2020년 법정동별 이용 금액");
+            primaryStage.setScene(scene);
+            primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        new DongDataAnalysis();
+        launch(args);
     }
 }
